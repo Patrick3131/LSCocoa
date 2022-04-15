5d6618436f08592f79ad710cf8970f7f83c12b28
Patrick Fischer <patrickfischer31@gmail.com>
2022-04-15 22:04:39 +0100

    Adds watchOS support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LSCocoa",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13), .watchOS(.v8)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["LSCocoa"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/dinogustinn/LSData.git", .branch("master"))
+        .package(url: "https://github.com/Patrick3131/LSData.git", .branch("dev_p"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
