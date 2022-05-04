df114fe4542c3eda6317b6ac6fa9bfb215bb51ec
Patrick3131 <patrick-fischer@live.de>
2022-05-04 16:08:43 +0100

    Implement logic for persistent history processor

--- Sources/LSCocoa/LSCoreData/PersistentHistoryProcessor/PersistentHistoryProcessor.swift
+++ Sources/LSCocoa/LSCoreData/PersistentHistoryProcessor/PersistentHistoryProcessor.swift
@@ -10,9 +10,13 @@ import CoreData
 
 
 public class PersistentHistoryProcessor: NSObject {
+    enum Error: Swift.Error {
+        case identicalIdentifiers
+        case historyTransactionConvertionFailed
+    }
     public struct Config {
-        let currentOriginator: PersistentDataOriginator
-        let userDefaults: UserDefaults
+        fileprivate let currentOriginator: PersistentDataOriginator
+        fileprivate let userDefaults: UserDefaults
         public init(currentOriginator: PersistentDataOriginator,
                     userDefaults: UserDefaults = .standard) {
             self.currentOriginator = currentOriginator
@@ -26,10 +30,7 @@ public class PersistentHistoryProcessor: NSObject {
         let allTargets = config.currentOriginator.allOrignators
         let idenfifiers = Set(allTargets.map { $0.identifier })
         if idenfifiers.count != allTargets.count {
-            throw NSError(domain: "PersistentHistoryMerger", code: 0, userInfo: [
-                "Info":"Please make sure you use different identifiers for your targets",
-                "identifiers": idenfifiers
-            ])
+            throw Error.identicalIdentifiers
         }
         self.config = config
     }
@@ -49,6 +50,8 @@ public class PersistentHistoryProcessor: NSObject {
                         notificationCenter: NotificationCenter) {
         mainContext.name = config.currentOriginator.identifier
         mainContext.transactionAuthor = config.currentOriginator.identifier
+        backgroundContext.name = config.currentOriginator.identifier
+        backgroundContext.transactionAuthor = config.currentOriginator.identifier
         self.backgroundContext = backgroundContext
         startObserving(container: container, notificationCenter: notificationCenter)
     }
@@ -65,16 +68,109 @@ public class PersistentHistoryProcessor: NSObject {
                 try merge()
                 try clean()
             } catch {
-                // print("Persistent History Tracking failed with error \(error)")
+                print("PersistentHistoryProcessor: failed with error \(error)")
             }
         }
     }
     
     private func merge() throws {
+        let fromDate = config.userDefaults.lastHistoryTransactionTimestamp(for: config.currentOriginator) ?? .distantPast
+        let history = try fetch(fromDate: fromDate)
         
+        guard !history.isEmpty else {
+            print("PersistentHistoryProcessor: No history transactions found to merge for target \(config.currentOriginator)")
+            return
+        }
+        
+        print("PersistentHistoryProcessor: Merging \(history.count) persistent history transactions for target \(config.currentOriginator)")
+        
+        if let context = backgroundContext {
+            history.merge(into: context)
+        }
+
+        
+        guard let lastTimestamp = history.last?.timestamp else { return }
+        config.userDefaults.updateLastHistoryTransactionTimestamp(for: config.currentOriginator, to: lastTimestamp)
     }
     
+    /// Cleans up the persistent history by deleting the transactions that have been merged into each originator.
     private func clean() throws {
-        
+        guard let timestamp = config.userDefaults.lastCommonTransactionTimestamp(in: config.currentOriginator.allOrignators) else {
+            print("PersistentHistoryProcessor: Cancelling deletions as there is no common transaction timestamp")
+            return
+        }
+
+        let deleteHistoryRequest = NSPersistentHistoryChangeRequest.deleteHistory(before: timestamp)
+        print("PersistentHistoryProcessor: Deleting persistent history using common timestamp \(timestamp)")
+        try backgroundContext?.execute(deleteHistoryRequest)
+
+        config.currentOriginator.allOrignators.forEach { target in
+            /// Reset the dates as we would otherwise end up in an infinite loop.
+            config.userDefaults.updateLastHistoryTransactionTimestamp(for: target, to: nil)
+        }
+    }
+    
+    private func fetch(fromDate: Date) throws -> [NSPersistentHistoryTransaction] {
+        let fetchRequest = createFetchRequest(fromDate: fromDate)
+        guard let historyResult = try backgroundContext?.execute(fetchRequest) as? NSPersistentHistoryResult, let history = historyResult.result as? [NSPersistentHistoryTransaction] else {
+            throw Error.historyTransactionConvertionFailed
+        }
+        return history
+    }
+    
+    private func createFetchRequest(fromDate: Date) -> NSPersistentHistoryChangeRequest {
+        let historyFetchRequest = NSPersistentHistoryChangeRequest
+                    .fetchHistory(after: fromDate)
+
+                if let fetchRequest = NSPersistentHistoryTransaction.fetchRequest {
+                    var predicates: [NSPredicate] = []
+                    if let transactionAuthor = backgroundContext?.transactionAuthor {
+                        /// Only look at transactions created by other targets.
+                        predicates.append(NSPredicate(format: "%K != %@",
+                                                      #keyPath(NSPersistentHistoryTransaction.author),
+                                                      transactionAuthor))
+                    }
+                    if let contextName = backgroundContext?.name {
+                        /// Only look at transactions not from our current context.
+                        predicates.append(NSPredicate(format: "%K != %@",
+                                                      #keyPath(NSPersistentHistoryTransaction.contextName),
+                                                      contextName))
+                    }
+                    fetchRequest.predicate = NSCompoundPredicate(type: .and, subpredicates: predicates)
+                    historyFetchRequest.fetchRequest = fetchRequest
+                }
+                return historyFetchRequest
+    }
+}
+
+fileprivate extension Collection where Element == NSPersistentHistoryTransaction {
+    
+    /// Merges the current collection of history transactions into the given managed object context.
+    /// - Parameter context: The managed object context in which the history transactions should be merged.
+    func merge(into context: NSManagedObjectContext) {
+        forEach { transaction in
+            guard let userInfo = transaction.objectIDNotification().userInfo else { return }
+            NSManagedObjectContext.mergeChanges(fromRemoteContextSave: userInfo, into: [context])
+        }
+    }
+}
+
+fileprivate extension UserDefaults {
+    
+    func lastHistoryTransactionTimestamp(for originator: PersistentDataOriginator) -> Date? {
+        let key = "lastHistoryTransactionTimeStamp-\(originator.identifier)"
+        return object(forKey: key) as? Date
+    }
+
+    func updateLastHistoryTransactionTimestamp(for originator: PersistentDataOriginator, to newValue: Date?) {
+        let key = "lastHistoryTransactionTimeStamp-\(originator.identifier)"
+        set(newValue, forKey: key)
+    }
+
+    func lastCommonTransactionTimestamp(in originators: [PersistentDataOriginator]) -> Date? {
+        let timestamp = originators
+            .map { lastHistoryTransactionTimestamp(for: $0) ?? .distantPast }
+            .min() ?? .distantPast
+        return timestamp > .distantPast ? timestamp : nil
     }
 }
